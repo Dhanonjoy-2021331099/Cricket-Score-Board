bed1cad70781bd30fc6490194b4c8fc53b40b5c9
Dhanonjoy-2021331099 <138089152+Dhanonjoy-2021331099@users.noreply.github.com>
2024-01-12 16:32:36 +0600

    Updated cricket_score_board.cpp

--- cricket_score_board.cpp
+++ cricket_score_board.cpp
@@ -9,6 +9,22 @@ int over = 0;
 int ball = 0;
 int wicket = 0;
 
+char content[255] = " ";
+
+char scoreStr[4];
+char wicketStr[2];
+char overStr[100];
+char ballStr[1];
+
+void createHTML() {
+    FILE *pFile = fopen("score11.html", "w");
+    fprintf(pFile, "<html><body><h3>");
+    fprintf(pFile, content);
+    fprintf(pFile, "</h3>");
+    fprintf(pFile, "</body></html>");
+    fclose(pFile);
+}
+
 void printLiveScore() {
     printf("\nLive Score:\n");
     printf("Score: %d / %d\n", run, wicket);
@@ -21,8 +37,13 @@ int main() {
     printf("Enter overs and wickets: ");
     scanf("%d%d", &o, &w);
     printf("%d overs & %d wickets", o, w);
+/* int x;
+          printf("\nEnter run range : ");
+          scanf("%d",&x);
+*/
     while (1) {
-       //printLiveScore();
+     printLiveScore();
+
 
         printf("\n Show live score, '0' press again ");
         scanf("%d", &score);
@@ -142,7 +163,7 @@ int main() {
                 run +=3;
                 ball +=1;
                 wicket +=1;
-                printf("\nWicket gone and triple run.\n");
+                printf("\nWicket gone and double run.\n");
                 break;
                 }
                 default :
@@ -224,29 +245,50 @@ int main() {
             default: {
                 printf("\nThis is an invalid input\n");
             }
+
+
+
         }
+
+        // convert integer to string and store string variable
+
+        sprintf(scoreStr, "%d", run);
+        sprintf(wicketStr, "%d", wicket);
+        sprintf(overStr, "%d", over);
+        sprintf(ballStr, "%d", ball);
+
+
+       strcat(content, " \n");
+        strcat(content, "Score: \n ");
+        strcat(content,  scoreStr);
+        strcat(content, " / ");
+        strcat(content, wicketStr);
+        strcat(content, " \n");
+        strcat(content, "\nOvers: ");
+        strcat(content, overStr);
+        strcat(content, ".");
+        strcat(content, ballStr);
+        strcat(content, "\n");
+
+        createHTML();
 
         if (ball == 6) {
             over += 1;
             ball = 0;
         }
         if (wicket == w) {
-                printf("\nWicket ended.\n");
-            printf("\nInnings Ended\n");
-            //printf("Target : %d",run+1);
+                 printf("Wicket Ended\n");
+            printf("Innings Ended\n");
+            printf("Target : %d",run+1);
             break;
         }
         if (over == o) {
-                printf("\nOver ended.\n");
-            printf("\nInnings Ended\n");
-            //printf("Target : %d",run+1);
+             printf("Over Ended\n");
+            printf("Innings Ended\n");
+            printf("Target : %d",run+1);
             break;
         }
-        printLiveScore();
-
-    }
-    printLiveScore();
-
-    printf("\nTarget : %d",run+1);
+       strcpy(content,"");
+      // system("cls");
 }
-
+}
